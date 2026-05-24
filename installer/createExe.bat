b56dec1fb97f321a32027f7b86a5dd06ffb4b4e2
Franco Flamigni <f.flamigni@geoin.it>
2026-05-24 16:20:19 +0200

    upfdated gitignore

--- installer/createExe.bat
+++ installer/createExe.bat
@@ -2,6 +2,7 @@ pyinstaller.exe --clean --noconfirm --windowed --noconsole --name Euterpe^
  --icon=..\icone\player.ico^
  --add-data="../icone/*;./icone/"^
  --add-data="../exe/;./exe/"^
+ --add-data="../docs/;./docs/"^
  --paths="../exe"^
  --paths "C:/Users/franc/MySoft/pyMyLib/"^
  ../Player.py
